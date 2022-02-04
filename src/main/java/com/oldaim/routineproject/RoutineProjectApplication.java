e2e74a19c7f57e375814c7c7e2e47042d7811d57
김동균 <dgk0911@gmail.com>
2022-02-04 14:06:11 +0900

    #1 - 프로젝트 초기 설정
    - Spring boot 초기 설정 및 h2 database 설정

---
+++ src/main/java/com/oldaim/routineproject/RoutineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.oldaim.routineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutineProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oldaim/routineproject/RoutineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oldaim.routineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
