2e9827a1c8c2db62dc75d067a88b08b50ac66a9b
Kimdongkyun <dgk0911@gmail.com>
2022-02-28 15:34:51 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,4 +30,4 @@ echo "배포 애플리케이션 로그 기록"
 
 echo "> JAR Name: $JAR_NAME"
 
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & -Dspring.config.location=classpath:/home/ec2-user/app/step2/application-real-db.yml
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & -Dspring.config.location=/home/ec2-user/app/step2/application-real-db.yml
