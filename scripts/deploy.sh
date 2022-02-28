cc46b2173cf41787eae05db499bce1fe34239d3f
Kimdongkyun <dgk0911@gmail.com>
2022-02-28 16:20:15 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,27 +4,29 @@ REPOSITORY=/home/ec2-user/app/step2
 
 PROJECT_NAME=RoutineProject
 
-echo "Build 파일 복사"
+echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+CURRENT_PID=$(pgrep -fl RoutineProject | grep jar | awk '{print $1}')
 
-echo " 현재 구동중인 애플리케이션pid: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-        echo "> kill -15 $CURRENT_PID"
+    echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
     sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
